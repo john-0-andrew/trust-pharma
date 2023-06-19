086d0b42b092148fcc337ecf52b457ee37507888
AGeorge556 <gn_farag02@outlook.com>
2023-06-20 02:29:40 +0300

    modified:   src/App.css 	modified:   src/App.jsx 	new file:   src/Components/HomePage/DNA/DNA.css 	new file:   src/Components/HomePage/DNA/DNA.jsx 	new file:   src/Components/HomePage/HomePage.css 	new file:   src/Components/HomePage/HomePage.jsx 	new file:   src/assets/logo.png 	modified:   src/index.css

---
+++ src/Components/HomePage/DNA/DNA.jsx
@@ -0,0 +1,212 @@
+import './DNA.css'
+
+function DNA() {
+  return (
+    <div>
+        <div className="helix" style={{"--total": 20}}>
+  <div className="strand" style={{"--i": 1}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 2}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 3}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 4}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 5}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 6}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 7}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 8}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 9}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 10}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 11}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 12}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 13}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 14}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 15}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 16}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 17}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 18}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 19}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+  <div className="strand" style={{"--i": 20}}>
+    <div className="left">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+    <div className="right">
+      <div className="dot"></div>
+      <div className="line"></div>
+    </div>
+  </div>
+</div>  
+    </div>
+  )
+}
+
+export default DNA
---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,21 @@
+import './HomePage.css'
+import DNA from './DNA/DNA'
+function HomePage() {
+  return (
+    <div className='section'>
+      <div className='dnh'>
+      <div className='header_text'>
+        <h1>
+        Welcome to Trust Pharma LTD
+          </h1>
+      </div>
+      <div className='dna'>
+      <DNA/>
+      </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default HomePage
