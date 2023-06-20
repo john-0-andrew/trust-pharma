198c7d8aa6baade5b1111512da03bd5f77a8ec91
johnsamuel20 <joe.samuel.john.js@gmail.com>
2023-06-20 15:52:47 +0300

    modified:   src/Components/HomePage/HomePage.css 	modified:   src/Components/HomePage/HomePage.jsx 	new file:   src/Components/NavButton/NavButton.jsx 	modified:   src/assets/logo.png

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import './HomePage.css'
 import DNA from './DNA/DNA'
 import { useRef } from 'react'
 import { useInView } from 'framer-motion'
+import logo from '../../assets/LOGO.png'
 function HomePage() {
   function Div({ children }) {
     const ref = useRef(null)
@@ -12,9 +13,9 @@ function HomePage() {
       <div ref={ref} >
         <h1 className='header_text'
           style={{
-            transform: isInView ? 'none' : 'translateX(-200px)',
+            transform: isInView ? 'none' : 'translateY(500px)',
             opacity: isInView ? 1 : 0,
-            transition: 'all 0.3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s',
+            transition: 'all 0.7s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s',
           }}
         >
           {children}
@@ -22,15 +23,34 @@ function HomePage() {
       </div>
     )
   }
+  function Section({ children }) {
+    const ref = useRef(null)
+    const isInView = useInView(ref, { once: true })
+
+    return (
+      <div ref={ref} className='dna' >
+        <div 
+          style={{
+            transform: isInView ? 'none' : 'translateY(500px)',
+            opacity: isInView ? 1 : 0,
+            transition: 'all 0.7s cubic-bezier(0.17, 0.55, 0.55, 1) 0.7s',
+          }}
+          >
+          {children}
+        </div>
+      </div>
+    )
+  }
   return (  
     <div className='section'>
+      <div className='border'> <img src={logo} alt="" /></div>
       <div className='dnh'>
       <Div>
       Welcome to Trust Pharma LTD
       </Div>
-      <div className='dna'>
+      <Section >
       <DNA/>
-      </div>
+      </Section>
       </div>
       
     </div>
