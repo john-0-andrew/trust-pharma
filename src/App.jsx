7c09287f50a5d5b7afd6abc20956043d53566c21
johnsamuel20 <joe.samuel.john.js@gmail.com>
2023-07-04 17:30:22 +0300

    modified:   src/App.jsx 	new file:   src/Components/Loading/Loading.css 	new file:   src/Components/Loading/Loading.jsx 	modified:   src/Components/NavMenu/NavMenu.css 	modified:   src/Components/Products3/Products3.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
-import './App.css'
-import AboutUs from './Components/AboutUs/AboutUs'
-import HomePage from './Components/HomePage/HomePage'
+import { useState, useEffect } from "react";
+import "./App.css";
+import AboutUs from "./Components/AboutUs/AboutUs";
+import HomePage from "./Components/HomePage/HomePage";
 import Products3 from "./Components/Products3/Products3";
 import Team from "./Components/Team/Team";
-import WhyTrust from './Components/WhyTrust/WhyTrust';
-import Contact from './Components/Contact/Contact';
+import WhyTrust from "./Components/WhyTrust/WhyTrust";
+import Contact from "./Components/Contact/Contact";
+import Loading from "./Components/Loading/Loading";
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsLoading(false);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 4000);
+    };
+
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
 
   return (
-    <div className='site'>
-    <HomePage/>
-    <AboutUs/>
-    {/* <Products/> */}
-    {/* <Products2/> */}
-    <Products3/>
-    <Team/>
-    <WhyTrust/>
-    <Contact/>
+    <div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="site">
+          <HomePage />
+          <AboutUs />
+          <Products3 />
+          <Team />
+          <WhyTrust />
+          <Contact />
+        </div>
+      )}
     </div>
-  )
+    // <div className="site">
+    //   <Loading />
+    // </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,18 @@
+import './Loading.css'
+
+function Loading() {
+  return (
+    <div className="loading-screen">
+      <div className="🤚">
+        <div className="👉"></div>
+        <div className="👉"></div>
+        <div className="👉"></div>
+        <div className="👉"></div>
+        <div className="🌴"></div>
+        <div className="👍"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading
