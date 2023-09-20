ff18a38b76cb7b8dd4a2bacaf55f7a0981707a34
johnsamuel20 <joe.samuel.john.js@gmail.com>
2023-09-20 15:09:55 +0300

    adding Team members desc

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -10,37 +10,49 @@ function Team() {
         "https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       name: "SomeOne Famous",
       position: "Director",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, earum. Ipsa, optio distinctio! Repellendus aperiam ex eum magnam alias nulla recusandae animi nihil possimus, magni, iure, vitae itaque aliquid neque.",
     },
     {
       memberImage:
         "https://images.unsplash.com/photo-1549417229-aa67d3263c09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       name: "SomeOne Famous",
       position: "Director",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, earum. Ipsa, optio distinctio! Repellendus aperiam ex eum magnam alias nulla recusandae animi nihil possimus, magni, iure, vitae itaque aliquid neque.",
     },
   ];
   const TeamCard = ({ teamData, children }) => {
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true });
-    const { memberImage, name, position} = teamData;
+    const { memberImage, name, position, description } = teamData;
     return (
-      <div
-        ref={ref}
-        className="card"
-        style={{
-          transform: isInView ? "none" : "translateX(-200px)",
-          opacity: isInView ? 1 : 0,
-          transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s",
-        }}
-      >
-        <div className="imgBx">
-          <img src={memberImage} alt="images" />
+      <div className="blog">
+        <div
+          ref={ref}
+          className="card"
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s",
+          }}
+        >
+          <div className="imgBx">
+            <img src={memberImage} alt="images" />
+            {/* <div className="black-tape"></div> */}
+          </div>
+          <div className="details">
+            <h2>
+              {name}
+              <br />
+              <span>{position}</span>
+            </h2>
+          </div>
         </div>
-        <div className="details">
-          <h2>
-            {name}
-            <br />
-            <span>{position}</span>
-          </h2>
+        <div className="card__content">
+          <span>{position}</span>
+          <hr className="solid" />
+          {description}
         </div>
       </div>
     );
@@ -51,11 +63,9 @@ function Team() {
         <h1>Our Team</h1>
       </div>
       <div className="box">
-        <div>
           {teamCardData.map((item, index) => (
             <TeamCard key={index} teamData={item} />
           ))}
-        </div>
       </div>
     </div>
   );
